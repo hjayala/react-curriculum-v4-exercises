ee95e23ab1ea999b77c0bfeadea2b2cf4f8d68e1
Hector Ayala <hjayala70@gmail.com>
2025-11-24 00:14:27 -0500

    Week 1 exercises

--- src/exercises/week-01/studentWork.jsx
+++ src/exercises/week-01/studentWork.jsx
@@ -3,10 +3,30 @@
 
 export default function StudentWork() {
   //add variables here 
+  const name = "Hector Ayala";
+  const age = 21;
+  const hobbies = [
+    { id: 1, title: "Coding" },
+    { id: 2, title: "Videogames" },
+    { id: 3, title: "Rollerblading" },
+    { id: 4, title: "Speedcubing" },
+    { id: 5, title: "Drawing" },
+    { id: 6, title: "Piano" }
+  ]
+
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>
+        HEY! {name.toUpperCase()} HERE!
+      </h1>
+      <p>
+        Hello there! My name is {name}! I am a {age} years old Computer Science BS 3rd year student at Barry University!
+      I was born in San Pedro Sula, Honduras. I have two cats and I love building computers!
+      </p>
+      <ul>
+          {hobbies.map(hobby => <li key={hobby.id}>{hobby.title}</li>)}
+      </ul>
     </div>
   );
 }
