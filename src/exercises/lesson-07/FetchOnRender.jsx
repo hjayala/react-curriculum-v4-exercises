5caa9814c5ff0bf4e6b1e3a960e880397c6d8908
Hector Ayala <hjayala70@gmail.com>
2026-05-20 20:43:04 -0400

    Lesson 07 completed

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from 'react';
+import { getPosts } from './api';
 import './Lesson07Styles.css';
 
 export default function FetchOnRender() {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsLoading(true);
+      try {
+        const data = await getPosts();
+        setPosts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchPosts();
+  }, []);
+
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
       <div className="content">
-        TODO: Replace me with fetched data when the component renders
+        {isLoading && <p>Loading posts...</p>}
+        {error && <p>{error}</p>}
+        {!isLoading &&
+          !error &&
+          posts.map((post) => (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
